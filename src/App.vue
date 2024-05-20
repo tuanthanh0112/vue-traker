cf54e4936025ffdfeebba28f9b9024f9008b66f1
Mac <>
2024-05-20 17:10:28 +0700

    change commit

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <Header />
+  <div class="container">
+    <Balance :total="total"/>
+    <IncomeExpenses :income="income" :expenses="expenses"/>
+    <TransationList :transactions="transactions"/>
+    <AddTransaction />
+  </div>
+
+</template>
+
+<script setup>
+  import Header from './components/Header.vue';
+  import Balance from './components/Balance.vue';
+  import IncomeExpenses from './components/IncomeExpenses.vue';
+  import TransationList from './components/TransationList.vue';
+  import AddTransaction from './components/AddTransaction.vue';
+
+  import { ref, computed } from 'vue'
+
+  const transactions = ref([
+    {id: 1, text:'Flower',amount: -19.99 },
+    {id: 2, text:'Salary',amount: 299.97 },
+    {id: 3, text:'Book',amount: -10 },
+    {id: 4, text:'Camera',amount: 150 },
+  ]);
+
+  const total = computed(() => {
+    return transactions.value.reduce((acc, transaction) => {
+      return acc + transaction.amount;
+    }, 0)
+  });
+
+  const income = computed(() => {
+    return transactions.value.
+    filter((transaction) => transaction.amount > 0)
+    .reduce((acc, transaction) => {
+      return acc + transaction.amount;
+    }, 0).toFixed(2);
+  });
+
+  const expenses = computed(() => {
+    return transactions.value.
+    filter((transaction) => transaction.amount < 0)
+    .reduce((acc, transaction) => {
+      return acc + transaction.amount;
+    }, 0).toFixed(2);
+  });
+  
+</script>
---
+++ src/components/TransationList.vue
@@ -0,0 +1,22 @@
+<template>
+    <h3>History</h3>
+    <ul id="list" class="list">
+        <li :class="transaction.amount > 0 ? 'minus' : 'plus'" v-for="transaction in transactions" :key="transaction.id">
+               {{ transaction.text }} <span>{{ transaction.amount}}</span><button class="delete-btn">x</button>
+        </li>
+      <!-- <li class="plus">
+        Paychck <span>-$400</span><button class="delete-btn">x</button>
+      </li> -->
+    </ul>
+</template>
+
+<script setup>
+    import {defineProps} from 'vue';
+
+    const props = defineProps({
+        transactions: {
+            type: Array,
+            required: true,
+        }
+    });
+</script>
